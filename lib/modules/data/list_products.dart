fdbd440c3ab5b01ceb2743b3ef66b7cc640389fd
ambitencourt <adriano.mirandabitencourt@gmail.com>
2022-02-01 12:32:38 -0300

    Version 1.0.0.1

--- lib/modules/data/list_products.dart
+++ lib/modules/data/list_products.dart
@@ -3,14 +3,15 @@ import 'package:ecommerce/shared/models/product_model.dart';
 final products = [
   Product(
     title: 'Xiaomi Redmi 9',
-    description: 'Aperto o botão de ligar e o computador não liga.',
+    description: 'Celular Xiaomi Redmi 9 Activ Dual 6Gb Ram 128Gb.',
     image: 'assets/products/Redmi9_thumb.jpg',
     price: 1299.00,
   ),
   Product(
     title: 'Impressora Epson L3250',
-    description: 'Aperto o botão de ligar e o computador não liga.',
+    description:
+        'Multifuncional Epson EcoTank L3250 - Tanque de Tinta Colorida, Wi-Fi Direct, USB, Bivolt.',
     image: 'assets/products/L3250_thumb.jpg',
-    price: 1299.00,
+    price: 1240.00,
   ),
 ];
